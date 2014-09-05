004accd84e1e1f5460b7ed85040bf2be2cf89c30
Arnout Kazemier <info@3rd-Eden.com>
2014-09-05 13:24:58 +0200

    [init] Initial functionality

---
+++ index.js
@@ -0,0 +1,248 @@
+'use strict';
+
+/**
+ * Find the index an object which has the given name.
+ *
+ * @param {Array} array Array to search for index.
+ * @param {String} name Name we should match.
+ * @returns {Number} index.
+ * @api private
+ */
+function index(array, name) {
+  for (var i = 0, l = array.length; i < l; i++) {
+    if (name === array[i].name) return i;
+  }
+
+  return -1;
+}
+
+/**
+ * Figure out if we're given a path to a file, a pre-read file or a function and
+ * return the string or source contents from it.
+ *
+ * @param {Buffer|String|Function} code Unknown code source.
+ * @returns {String}
+ * @api private
+ */
+function toSource(code) {
+  if ('string' !== typeof code) return code.toString('utf-8');
+
+  //
+  // Now for the hard part, we need to determine if we've been given an absolute
+  // path to a file or pre-read file. We can assume that `//` and `/*` are
+  // indications for code starting with comments and that absolute paths start
+  // with / or (require(path).sep).
+  //
+  var comment = [47, 42];
+
+  if (code.charCodeAt(0) !== 47 || !~comment.indexOf(code.charCodeAt(1))) {
+    return require('fs').readFileSync(code, 'utf-8');
+  }
+
+  return code;
+}
+
+/**
+ * Representation of a single middleware layer.
+ *
+ * @constructor
+ * @param {String} name Identification of the middleware.
+ * @param {Function} fn Middleware function.
+ * @param {Mixed} context Execution context of the function.
+ * @api private
+ */
+function Layer(name, fn, context) {
+  this.length = fn.length;
+  this.context = context;
+  this.name = name;
+  this.fn = fn;
+}
+
+/**
+ * Representation of a plugin specification.
+ *
+ * @param {String} name Identification of the plugin.
+ * @param {Object} obj Plugin representation.
+ * @param {Mixed} context Execution context of the server.
+ * @api private
+ */
+function Specification(name, obj, context) {
+  this.library = toSource(obj.library);
+  this.client = toSource(obj.client);
+  this.length = obj.server.length;
+  this.context = context;
+  this.fn = obj.server;
+  this.name = name;
+}
+
+function Supply() {
+  /* Pointless function, like an empty glass of beer. */
+}
+
+Supply.prototype.__proto__ = require('eventemitter3').prototype;
+
+/**
+ * @param {String} name Name of the layer we wish to add.
+ * @param {Object} obj Specification of the plugin.
+ */
+Supply.prototype.plugin = function plugin(name, obj, options) {
+  if (!this._plugin) this._plugin = [];
+  if (!obj) return this._plugin[index(this._plugin, name)];
+
+  options = options || {};
+
+  options.index = 'index' in options ? +options.index : this._before.length;
+  options.context = options.context || this;
+
+  if (options.before) options.index = index(this._before, options.before);
+  if (options.index > this._before.length) options.index = this._before.length;
+
+  var spec = new Specification(name, obj, options.context);
+
+  this.emit('plugin', spec, options);
+  this._plugin.splice(options.index, 0, spec);
+
+  return this;
+};
+
+/**
+ * Removes the plugin from the stack.
+ *
+ * @param {String} name Name of the layer we wish to remove.
+ * @returns {Boolean} Indication of successful removal.
+ * @api public
+ */
+Supply.prototype.unplug = Supply.prototype.plugout = function unplug(name) {
+  var i = index(this._plugin, name);
+  if (i === -1) return false;
+
+  this.emit('unplug', this._plugin.splice(i, 1));
+  return true;
+};
+
+/**
+ * Add or retrieve a middleware layer. If no function argument is given we
+ * assume that you want to retrieve a middleware layer that matches the given
+ * name.
+ *
+ * If you do supply it with a function argument we assume that you wish to
+ * register this middleware layer for the given name. When adding a new layer
+ * you can supply the following options:
+ *
+ * - `index` The position of the middleware layer where we should add it.
+ * - `before` Name of a middleware layer that you want to run this before.
+ * - `context` Function execution context.
+ *
+ * @param {String} name Name of the layer we wish to add.
+ * @param {Function} fn Middleware layer.
+ * @param {Object} options Middleware layer configuration.
+ * @returns {Supply|Layer}
+ * @api public
+ */
+Supply.prototype.before = function before(name, fn, options) {
+  if (!this._before) this._before = [];
+  if (!fn) return this._before[index(this._before, name)];
+
+  options = options || {};
+
+  options.index = 'index' in options ? +options.index : this._before.length;
+  options.context = options.context || this;
+
+  if (options.before) options.index = index(this._before, options.before);
+  if (options.index > this._before.length) options.index = this._before.length;
+
+  var layer = new Layer(name, fn, options);
+
+  this.emit('before', layer, options);
+  this._before.splice(options.index, 0, layer);
+
+  return this;
+};
+
+/**
+ * Removes the middleware from the stack.
+ *
+ * @param {String} name Name of the layer we wish to remove.
+ * @returns {Boolean} Indication of successful removal.
+ * @api public
+ */
+Supply.prototype.remove = function remove(name) {
+  var i = index(this._plugin, name);
+  if (i === -1) return false;
+
+  this.emit('unplug', this._plugin.splice(i, 1));
+  return true;
+};
+
+/**
+ * Iterate and execute all middleware layers with the given set of arguments.
+ *
+ * @param {String} what Before or plugin we're executing.
+ * @Argument {Mixed} .. Arguments that need to be supplied to the layers.
+ * @param {Function} fn Completion callback.
+ * @returns {Supply}
+ * @api public
+ */
+Supply.prototype.each = function each(what, a, b, c, d) {
+  what = '_'+ what;
+  if (!this[what] || !this[what].length) return fn(), supply;
+
+  //
+  // Create a copy of the arguments to prevent argument leaking in the loop
+  // closure.
+  //
+  for (var i = 1, l = arguments.length - 1, args = new Array(l); i < l; i++) {
+    args[i - 1] = arguments[i];
+  }
+
+  var fn = arguments[l]
+    , supply = this;
+
+  var async = args.length + 1
+    , layers = supply[what];
+
+  (function loop(i) {
+    if (i === layers.length) return fn();
+
+    var layer = layers[i]
+      , async = layer.length === l;
+
+    function next(err) {
+      if (err) return fn(err);
+      loop(i++);
+    }
+
+    //
+    // Optimize 1, 2 and 3 arguments. All others will get a bigger performance
+    // hit.
+    //
+    switch (l) {
+      case 1:
+        if (async) return layer.fn.call(layer.context, a, next);
+        return layer.fn.call(layer.context, a) !== true && loop(i++);
+      case 2:
+        if (async) return layer.fn.call(layer.context, a, b, next);
+        return layer.fn.call(layer.context, a, b) !== true && loop(i++);
+      case 3:
+        if (async) return layer.fn.call(layer.context, a, b, c, next);
+        return layer.fn.call(layer.context, a, b, c) !== true && loop(i++);
+    }
+
+    //
+    // This is a much slower layer invocation as we have to use function.apply
+    // to get the correct argument set and with an async function we need to
+    // concat the arguments so we can correctly add the next callback.
+    //
+    if (async) return layer.fn.apply(layer.context, args.concat(next));
+    return layer.fn.apply(layer.context, args) !== true && loop(i++);
+  })(0);
+
+  return supply;
+};
+
+//
+// Expose the module.
+//
+Supply.Specification = Specification;
+Supply.Layer = Layer;
+module.exports = Supply;
---
+++ test.js
@@ -0,0 +1,100 @@
+describe('supply', function () {
+  'use strict';
+
+  var assume = require('assume')
+    , Supply = require('./');
+
+  it('is exported a function', function () {
+    assume(Supply).to.be.a('function');
+  });
+
+  it('is an EventEmitter3', function () {
+    var s = new Supply();
+
+    assume(s).to.be.instanceOf(require('eventemitter3'));
+  });
+
+  describe('.before', function () {
+    it('emits a `before` event when adding a layer', function (next) {
+      var s = new Supply();
+
+      s.once('before', function (layer) {
+        assume(layer).to.be.instanceOf(Supply.Layer);
+        assume(layer.name).to.equal('bar');
+
+        next();
+      });
+
+      assume(s.before('bar', function (bar) {})).to.equal(s);
+    });
+
+    it('receives the supplied options in the `before` event', function (next) {
+      var s = new Supply();
+
+      s.once('before', function (layer, options) {
+        assume(options).to.be.a('object');
+        assume(options.foo).to.equal('bar');
+        assume(options.index).to.be.a('number');
+        assume(options.index).to.equal(0);
+
+        next();
+      });
+
+      s.before('bar', function (bar) {}, {
+        foo: 'bar'
+      });
+    });
+
+    it('returns the layer if no function is supplied', function () {
+      var s = new Supply();
+      assume(s.before('bar', function (bar) {})).to.equal(s);
+
+      var layer = s.before('bar');
+
+      assume(layer).to.be.instanceOf(Supply.Layer);
+      assume(layer.name).to.equal('bar');
+    });
+
+    it('adds the middleware layers in order of specification', function () {
+      var s = new Supply();
+
+      s.before('bar', function (bar) {});
+      s.before('foo', function (foo) {});
+
+      assume(s._before[0].name).to.equal('bar');
+      assume(s._before[1].name).to.equal('foo');
+    });
+
+    it('allows specifiying a custom index', function () {
+      var s = new Supply();
+
+      s.before('bar', function (bar) {});
+      s.before('foo', function (foo) {}, { index: 0 });
+
+      assume(s._before[0].name).to.equal('foo');
+      assume(s._before[1].name).to.equal('bar');
+    });
+
+    it('normalizes the index when its out of bound', function () {
+      var s = new Supply();
+
+      s.before('bar', function (bar) {});
+      s.before('foo', function (foo) {}, { index: 100 });
+
+      assume(s._before).to.have.length(2);
+    });
+
+    it('allows adding before a different middleware', function () {
+      var s = new Supply();
+
+      s.before('bar', function (bar) {});
+      s.before('foo', function (foo) {}, { before: 'bar' });
+
+      assume(s._before[0].name).to.equal('foo');
+      assume(s._before[1].name).to.equal('bar');
+    });
+
+    it('stores the custom context');
+    it('allows modification of the layer during `before` event');
+  });
+});
